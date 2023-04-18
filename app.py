9dd094c7ab135e8bd256ada2f58a680660accaab
Alexanthe <81797141+Alexanthe@users.noreply.github.com>
2023-04-18 23:27:31 +0800

    Added in about.html and edited app.py for inheritance

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# set FLASK_APP = app.py
-# $env:FLASK_ENV = "development"  # ON debug mode
-# flask run
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -15,7 +11,7 @@ def home_page():
 
 @app.route("/about")
 def about():
-    return "About this project.."
+    return render_template("about.html")
 
 if (__name__) == "__main__":
     app.run()
