57e0e4c72c045dfcb1a3d3d2b481ee6691d3945c
Alexanthe <81797141+Alexanthe@users.noreply.github.com>
2023-05-08 15:45:08 +0800

    Remove space

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template("index.html")
-
 
 @app.route("/about")
 def about_page():
