961213150462d58477a944d1c52edca1c517c6d5
Alexanthe <81797141+Alexanthe@users.noreply.github.com>
2023-05-09 11:14:39 +0800

    Added in jinja linking between app.py and index.html

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+from beeai import *
+
+#Retrive WordDict
+wordDict = getwordDict()
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template("index.html")
+    word = getWord(wordDict)
+    
+    return render_template("index.html", word=word)
 
 @app.route("/about")
 def about_page():
